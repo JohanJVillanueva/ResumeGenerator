5773f7305c789fe474aa33b498ecf30c5eda5359
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-07-06 22:52:18 +0800

    I fixed the json file to not have arrays of information, instead they are in one whole array not separated as I'm not still sure how to loop through a json array, I also added code on form1 will try to see if it works

--- Resume/Form1.cs
+++ Resume/Form1.cs
@@ -1,10 +1,30 @@
+using System.Text.Json;
 namespace Resume
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
+
             InitializeComponent();
+            string filename = "resumeinfo.json";
+            string jsonstring = File.ReadAllText(filename);
+            Resume resume = JsonSerializer.Deserialize<Resume>(jsonstring)!;
+
+            MessageBox.Show(resume.firstname);
+            //MessageBox.Show($"TemperatureCelsius: {weatherForecast.TemperatureCelsius}");
+            //MessageBox.Show($"Summary: {weatherForecast.Summary}");
+
+
         }
+    }
+    public class Resume
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+
+
     }
 }
